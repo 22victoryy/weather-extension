ce2064daedb34ef758f0b8eb8ac68a2a087aae04
vchanyoung <dk90801@gmail.com>
2019-09-03 02:18:58 -0400

    weather: fixed api call

--- app/weather/weather.js
+++ app/weather/weather.js
@@ -7,15 +7,17 @@ window.addEventListener('load', function() {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const api = `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${long}&cnt=1&appid=9127a894a9e2c98639ed5ca00991a2c7`;
+      // const api = `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${long}&cnt=1&appid=9127a894a9e2c98639ed5ca00991a2c7`;
       //api needs change
+      const proxy = "https://cors-anywhere.herokuapp.com/"
+      const api = `${proxy}https://api.darksky.net/forecast/68caf3cad151571d05b5843c6cb4eb88/${lat},${long}`;
       fetch(api)
         .then (weather => {
           return weather.json();
         })
         .then(data => {
           console.log(data)
-          const {} = data.list
+          // const {} = data.list
         })
     });
   } else {
