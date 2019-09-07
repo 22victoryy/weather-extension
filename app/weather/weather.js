deffc169f1d00d70707b800dcc23f940c3da598b
vchanyoung <dk90801@gmail.com>
2019-09-07 04:16:48 -0400

    weather: fixed double api call and applied timeout to api call

--- app/weather/weather.js
+++ app/weather/weather.js
@@ -1,4 +1,3 @@
-// By victorchanyoung.cho@mail.utoronto.ca
 // September 3rd, 2019
 
 var options = {
@@ -7,20 +6,18 @@ var options = {
   maximumAge: 0
 };
 
-var success = function(pos) {
-  var crd = pos.coords;
-
-  console.log('Your current position is:');
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
-
 var error = function(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+// var success = function(pos) {
+//   var crd = pos.coords;
+
+//   console.log('Your current position is:');
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude: ${crd.longitude}`);
+//   console.log(`More or less ${crd.accuracy} meters.`);
+// }
 
 var getDate = function () {
   var today = new Date();
@@ -45,58 +42,79 @@ var setIcons = function(icon, iconID) {
 };
 
 window.addEventListener('load', function() {
+  var CurrentDate = document.querySelector(".Date");
+  var location = document.querySelector(".Location-Timezone");
   var temperatureDescription = document.querySelector(".Temperature-Description");
   var temperatureDegree = document.querySelector(".Degree");
-  var location = document.querySelector(".Location-Timezone");
   var temperatureSection = document.querySelector(".Temperature-Section");
   const temperaturespan = document.querySelector(".Temperature-Section span");
-  var CurrentDate = document.querySelector(".Date");
 
   CurrentDate.textContent = getDate();
 
-
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
+
+    // Obtain geolocation
+    var geoloc = navigator.geolocation.getCurrentPosition(position => {
       var long = position.coords.longitude;
       var lat = position.coords.latitude;
-
       // const api = `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${long}&cnt=1&appid=9127a894a9e2c98639ed5ca00991a2c7`;
-      //api needs change
       const proxy = "https://cors-anywhere.herokuapp.com/"
       const api = `${proxy}https://api.darksky.net/forecast/68caf3cad151571d05b5843c6cb4eb88/${lat},${long}`;
 
+      // Fetch weather data
       fetch(api)
-        .then (weather => {
-          return weather.json();
-        })
-        .then(data => {
-          console.log(data)
+      .then (weather => {
+        return weather.json();
+      })
+      .then(data => {
+        const {temperature, summary, icon} = data.currently;
 
-          const {temperature, summary, icon} = data.currently;
-          // Set DOM elements from API
-          // console.log(temperature)
-          // console.log(summary)
-          temperatureDegree.textContent = temperature;
-          temperatureDescription.textContent = summary;
-          location.textContent = data.timezone;
-          // set icon
-          setIcons(icon, document.querySelector(".icon"));
-          //change to celsius, seperate to func
-          temperatureSection.addEventListener("click", () => {
-            if (temperaturespan.textContent === "F") {
-              temperaturespan.textContent = "C";
-              temperatureDegree.textContent = ToCelsius(temperature);
-            } else {
-              temperaturespan.textContent = "F";
-              temperatureDegree.textContent = temperature;
-            }
+        // Set DOM elements from API
+        if (summary.includes("Cloudy")) {
+          document.getElementById('myVideo').setAttribute("src", "../videos/cloud.mp4");
+          document.getElementById('myVideo').load();
+          document.getElementById('myVideo').play();
+        }
+        else if (summary.includes("Drizzle")) {
+          document.getElementById('myVideo').setAttribute("src", "../videos/rain.mp4");
+          document.getElementById('myVideo').load();
+          document.getElementById('myVideo').play();
+        }
+        else if (summary.includes("Snow")) {
+          document.getElementById('myVideo').setAttribute("src", "../videos/snowflake.mp4");
+          document.getElementById('myVideo').load();
+          document.getElementById('myVideo').play();
+        }
+        else {
+          document.getElementById('myVideo').setAttribute("src", "../videos/sun.mp4");
+          document.getElementById('myVideo').load();
+          document.getElementById('myVideo').play();
+        }
 
-          });
-        })
+        temperatureDegree.textContent = temperature;
+        temperatureDescription.textContent = summary;
+        location.textContent = data.timezone;
+
+        // Set icons
+        setIcons(icon, document.querySelector(".icon"));
+
+        // Convert to celsius on click
+        temperatureSection.addEventListener("click", () => {
+          if (temperaturespan.textContent === "F") {
+            temperaturespan.textContent = "C";
+            temperatureDegree.textContent = ToCelsius(temperature);
+          } else {
+            temperaturespan.textContent = "F";
+            temperatureDegree.textContent = temperature;
+          }
+        });
+      });
+      navigator.geolocation.getCurrentPosition(geoloc, error, options);
     });
   } else {
     console.log("Please install live service to simulate environment.")
-  }
+  };
 });
+
 
 
